1b22f700de8de3b6adc18a1188bfb3cdc5674b39
Weijia Li <143468735+vvvicky123@users.noreply.github.com>
2024-09-20 17:47:54 +0800

    Update swim.py

--- swim.py
+++ swim.py
@@ -18,7 +18,7 @@ def calculate_normal_dist_params(data):
 
 # 创建直方图
 def create_histogram(data, name):
-    fig = px.histogram(data, x=data, nbins=30, title=f'Histogram for {name}')
+    fig = px.histogram(data, x=data, nbins=30, title=f'{name}')
     fig.update_layout(bargap=0.3, bargroupgap=0.2)
     return fig
 
@@ -27,7 +27,7 @@ def create_normal_dist_curve(data, mean, std):
     x = np.linspace(min(data), max(data), 100)
     y = (1 / (std * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mean) / std) ** 2)
     fig = go.Figure(data=[go.Scatter(x=x, y=y, mode='lines', name='Normal Dist Curve')])
-    fig.update_layout(title='Normal Distribution Curve')
+    fig.update_layout(title='夺冠期正态分布图')
     return fig
 
 # Streamlit 应用
@@ -44,33 +44,24 @@ with tabw:
     swim_women.insert(0, 'Index', range(1, len(swim_women) + 1))
     st.dataframe(swim_women[["名字", '国家','出生年份','世界冠军','奥运会','世锦赛','夺冠周期']])
 
-men_mean_age, men_std_age = calculate_normal_dist_params(swim_men['首冠年龄'])
+men_mean_age, men_std_age = calculate_normal_dist_params(swim_men['夺冠期'])
 histogram_men = create_histogram(swim_men['首冠年龄'], '男子游泳世界冠军首冠年龄分布')
 normal_dist_curve_men = create_normal_dist_curve(swim_men['夺冠期'], men_mean_age, men_std_age)
 
-mean_age, std_age = calculate_normal_dist_params(swim_women['首冠年龄'])
+mean_age, std_age = calculate_normal_dist_params(swim_women['夺冠期'])
 histogram_women = create_histogram(swim_women['首冠年龄'], '女子游泳运动员首次世界冠军年纪')
 normal_dist_curve_women = create_normal_dist_curve(swim_women['夺冠期'], mean_age, std_age)
 
-col1, col2 = st.columns(2)
+st.subheader("男子游泳运动员夺冠周期")
+tabm1, tabw1 = st.tabs(["首冠年龄分布柱状图", "夺冠周期分布","男子TOP60夺冠周期表"])
 
-with col1:
-    st.subheader("男子游泳运动员夺冠周期")
-    tabm1, tabm2, tabm3 = st.tabs(["首冠年龄分布柱状图", "夺冠周期分布","男子TOP60夺冠周期表"])
-    
-    # 在第一个标签页中显示直方图
-    with tabm1:
-        st.plotly_chart(histogram_men)
-    
-    # 在第二个标签页中显示正态分布曲线
-    with tabm2:
-        st.plotly_chart(normal_dist_curve_men)
-    
-    # 在第三个标签页中显示组合图
-    with tabm3:
-        st.dataframe(swim_men[["名字", '国家','出生年份','首冠年龄','首冠年份','末冠年份','夺冠期']])
+# 在第一个标签页中显示直方图
+with tabm1:
+    st.plotly_chart(histogram_men)
+    st.plotly_chart(normal_dist_curve_men)
+    st.dataframe(swim_men[["名字", '国家','出生年份','首冠年龄','首冠年份','末冠年份','夺冠期']])
 
-
-st.plotly_chart(histogram_women)
-st.plotly_chart(normal_dist_curve_women)
-st.plotly_chart(create_combined_plot(histogram_women, normal_dist_curve_women))
+with tabw1:
+    st.plotly_chart(histogram_women)
+    st.plotly_chart(normal_dist_curve_women)
+    st.plotly_chart(swim_women[["名字", '国家','出生年份','首冠年龄','首冠年份','末冠年份','夺冠期']])
